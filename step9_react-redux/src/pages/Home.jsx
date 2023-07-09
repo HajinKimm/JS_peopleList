4a72812cd318f05d6ee2925b39d3dc6b89f1452a
HajinKimm <11rlagkwls@naver.com>
2023-07-09 21:55:47 +0900

    명단 전체 삭제버튼 경고문 추가

--- step9_react-redux/src/pages/Home.jsx
+++ step9_react-redux/src/pages/Home.jsx
@@ -13,6 +13,15 @@ const Home = () => {
     const onIsOpen = ()=>{
         setIsOpen(!isOpen)
     }
+    const  useConfirm =(msg)=>{
+        const confirmAction = ()=>{
+            if(window.confirm(msg)){
+                dispatch(allRemoverStaff())
+            }
+        }
+        return confirmAction
+    }
+    const onConfirmDelete = useConfirm('삭제 시 명단을 되돌릴 수 없습니다. 삭제 하시겠습니까?')
     return (
         <SttafList>
             <div className="inner">
@@ -20,7 +29,7 @@ const Home = () => {
                 <div className="btnWrap">
                     {
                         staffathority?
-                        <button className='btn btnDel' onClick={()=>dispatch(allRemoverStaff())} >명단 전체 삭제</button>
+                        <button className='btn btnDel' onClick={(onConfirmDelete)} >명단 전체 삭제</button>
                         : <button className='btn btnDel' onClick={()=>navigate('/warning')}> 명단 전체 삭제</button>
                     }
                     <button className='btn btnShow' onClick={onIsOpen}>{isOpen? '명단 보기' : '명단 숨기기'}</button>
