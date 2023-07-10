13d6ba4c611b52cae281fabae1b90020e95733f3
HajinKimm <11rlagkwls@naver.com>
2023-07-10 09:55:10 +0900

    build testFile

--- step9_react-redux/src/App.jsx
+++ step9_react-redux/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import GlobalStyle from './styled/Global';
